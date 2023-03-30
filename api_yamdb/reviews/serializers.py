ad47e6e638d13909606ba6e91c41fe28a208ed76
Дмитрий Русаков <dmitriyrusakov51@gmail.com>
2023-03-30 20:50:56 +0700

    Подружил ревьюс с Тайтлами на базовом уровне

---
+++ api_yamdb/reviews/serializers.py
@@ -0,0 +1,131 @@
+from rest_framework import serializers 
+from reviews.models import *
+import re
+from django.utils import timezone
+from django.shortcuts import get_object_or_404
+
+class CategorySerializer(serializers.ModelSerializer):
+    """Сериализатор модели Category."""
+    def validate_slug(self, value):
+        """Проверка соответствия слага категории."""
+        if not re.fullmatch(r'^[-a-zA-Z0-9_]+$', value):
+            raise serializers.ValidationError(
+                'Псевдоним категории не соотвествует формату',
+            )
+        return value
+
+    class Meta: 
+        model = Category 
+        fields = ('name', 'slug')
+        lookup_field = 'slug'
+
+
+class GenreSerializer(serializers.ModelSerializer): 
+    """Сериализатор модели Genre."""
+
+    def validate_slug(self, value):
+        """Проверка соответствия слага жанра."""
+        if not re.fullmatch(r'^[-a-zA-Z0-9_]+$', value):
+            raise serializers.ValidationError(
+                'Псевдоним жанра не соотвествует формату',
+            )
+        return value
+
+    class Meta:
+        fields = ('name', 'slug')
+        model = Genre
+        lookup_field = 'slug' 
+
+
+class RatingRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.score
+
+class TitleSerializer(serializers.ModelSerializer):
+    """Базовый сериализатор модели Title."""
+
+    # rating = serializers.IntegerField(read_only=True)
+    rating = RatingRelatedField(read_only=True)
+
+    def validate_year(self, value):
+        """Проверка года на будущее время."""
+        current_year = timezone.now().year
+        if value > current_year:
+            raise serializers.ValidationError(
+                'Марти, ты опять взял Делориан без спроса?!',
+            )
+        return value
+
+    class Meta:
+        model = Title
+        fields = (
+            'id',
+            'name',
+            'year',
+            'description',
+            'genre',
+            'category',
+            'rating',
+        )
+
+
+class TitleReadSerializer(TitleSerializer):
+    """Сериализатор модели Title для чтения."""
+    genre = GenreSerializer(read_only=True, many=True)
+    category = CategorySerializer(read_only=True)
+
+
+class TitleWriteSerializer(TitleSerializer):
+    """Сериализатор модели Title для записи."""
+
+    category = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Category.objects.all(),
+        required=False,
+    )
+    genre = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Genre.objects.all(),
+        many=True,
+        required=False,
+    )
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username')
+    review = serializers.SlugRelatedField(
+        read_only=True, slug_field='text')
+
+    class Meta:
+        fields = ('id', 'text', 'author', 'pub_data',)
+        model = Comment
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username')
+
+    title = serializers.SlugRelatedField(
+        read_only=True, slug_field='name')
+
+    class Meta:
+        fields = ('id', 'text', 'author', 'score', 'pub_data')
+        model = Review
+
+    def validate_score(self, value):
+        if not 0 < value < 11:
+            raise serializers.ValidationError('Оценка по 10 бальной шкале!')
+        return value
+
+    def validate(self, data):
+        request = self.context['request']
+        author = request.user
+        title_id = self.context['view'].kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        if request.method == 'POST' and Review.objects.filter(
+                title=title, author=author).exists():
+            raise serializers.ValidationError(
+                'Вы уже оставили отзыв к этому произведению!'
+            )
+        return data
--- api_yamdb/reviews/views.py
+++ api_yamdb/reviews/views.py
@@ -1,3 +1,97 @@
-from django.shortcuts import render
+from reviews.models import *
+from rest_framework import viewsets, filters, mixins
+from rest_framework.permissions import SAFE_METHODS
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import LimitOffsetPagination
+from .serializers import *
+from .filters import TitleFilter
+from .permissions import IsAdminOrReadOnly, AdminModeratorAuthorPermission
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+class CreateListDestroyViewSet(mixins.CreateModelMixin,
+                               mixins.ListModelMixin,
+                               mixins.DestroyModelMixin,
+                               viewsets.GenericViewSet):
+    """Дженерик для операций retrieve/create/list."""
+
+    lookup_field = 'slug'
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+
+
+class TitleViewSet(viewsets.ModelViewSet): 
+    queryset = Title.objects.all() 
+    serializer_class = TitleSerializer 
+    pagination_class = LimitOffsetPagination 
+    permission_classes = (
+        # IsAuthenticatedOrReadOnly,
+        IsAdminOrReadOnly,
+    )
+
+
+class CategoryViewSet(CreateListDestroyViewSet):
+    """Вьюсет для модели Category."""
+
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (
+        # IsAuthenticatedOrReadOnly,
+        IsAdminOrReadOnly,
+    )
+
+class GenreViewSet(CreateListDestroyViewSet):
+    """Вьюсет для модели Genre."""
+
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (
+        # IsAuthenticatedOrReadOnly,
+        IsAdminOrReadOnly,
+    )
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    """Вьюсет для модели Title."""
+    queryset = Title.objects.all()
+    
+    # queryset = Title.objects.annotate(
+    #     rating=Avg('reviews__score'),
+    # ).order_by('name')
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TitleFilter
+    permission_classes = (
+        # IsAuthenticatedOrReadOnly,
+        IsAdminOrReadOnly,
+    )
+
+    def get_serializer_class(self):
+        if self.request.method in SAFE_METHODS:
+            return TitleReadSerializer
+        return TitleWriteSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    permisson_classes = (AdminModeratorAuthorPermission,)
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
+        serializer.save(author=self.request.user, review=review)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    pagination_class = LimitOffsetPagination
+    permisson_class = (AdminModeratorAuthorPermission,)
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        serializer.save(author=self.request.user, title=title)
