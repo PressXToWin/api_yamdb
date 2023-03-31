b44268252feea14fb1dfb25c4bd3dd1d9ff279f6
Дмитрий Русаков <dmitriyrusakov51@gmail.com>
2023-03-31 07:31:55 +0700

    теперь здесь тоже 11 ошибок

--- api_yamdb/reviews/permissions.py
+++ api_yamdb/reviews/permissions.py
@@ -5,11 +5,14 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     """Даёт доступ неадмину только к GET/OPTIONS/HEAD."""
     
     message = 'Данный запрос недоступен для вас.'
+
     def has_permission(self, request, view):
         """Проверка на запросы к объекту
         Для безопасных методов всегда True."""
         return (request.method in permissions.SAFE_METHODS
-                or (request.user.role == 'admin'))
+                or (request.user.is_authenticated and request.user.role == 'admin'))
+  
+
 
 class AdminModeratorAuthorPermission(permissions.BasePermission):
     """Редактирование только автору, модератору и админу."""
