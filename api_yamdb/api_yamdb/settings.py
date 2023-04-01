33479cf4cb17bc6d1d1b372dbf222c62c97f7ad9
PressXToWin <PressXToWin@users.noreply.github.com>
2023-04-01 11:02:43 +0700

    разрешение конфликтов

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_filters',
     'rest_framework_simplejwt',
     'django_filters',
     'reviews.apps.ReviewsConfig',
